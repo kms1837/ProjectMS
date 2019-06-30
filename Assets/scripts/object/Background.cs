96e0e6329252a2d14365aad5b08b282dc8dfebd7
kms1837 <kms1837@gmail.com>
2019-07-01 04:37:30 +0900

    add background

---
+++ Assets/scripts/object/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private Transform sky;
+    private Material material;
+    public float scrollSpeed;
+
+    void Start() {
+        sky = this.transform.Find("Sky");
+        material = sky.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0);
+
+    }
+}
--- Assets/scripts/scene/InGameManager.cs
+++ Assets/scripts/scene/InGameManager.cs
@@ -8,6 +8,9 @@ public class InGameManager : MonoBehaviour {
     private GameObject enemyGroup;
     private int currentLayer; // 시작레이어(0-밖, 1-안)
 
+    public Transform cameraTarget; // 카메라가 따라다닐 대상
+    private GameObject background;
+
     private void childsLayerActive(GameObject group) {
         foreach (Transform child in group.transform) {
             if (child.GetComponent<Character>().layer == currentLayer) {
@@ -30,6 +33,11 @@ public class InGameManager : MonoBehaviour {
         indoor.SetActive(outdoor.activeSelf ? true : false);
         outdoor.SetActive(outdoor.activeSelf ? false : true);
 
+        Color backgroundColor;
+        backgroundColor = outdoor.activeSelf ? new Color(0.33f, 0.58f, 0.98f) : new Color(0,0,0);
+        Camera.main.backgroundColor = backgroundColor;
+        background.SetActive(outdoor.activeSelf);
+
         currentLayer = currentLayer == 1 ? 0 : 1;
         childsLayerActive();
     }
@@ -38,10 +46,12 @@ public class InGameManager : MonoBehaviour {
         Map = GameObject.Find("Map");
         npcGroup = GameObject.Find("NpcGroup");
         enemyGroup = GameObject.Find("EnemyGroup");
+
+        background = Camera.main.transform.Find("Background").gameObject;
         childsLayerActive();
     }
 
 	void Update () {
-		
-	}
+        Camera.main.transform.position = cameraTarget.position - Vector3.forward;
+    }
 }
