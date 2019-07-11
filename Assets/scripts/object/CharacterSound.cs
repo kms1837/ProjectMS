e53b693394a37527ae63ff356615a062eb6e0856
kms1837 <kms1837@gmail.com>
2019-07-12 04:10:29 +0900

    add effect sound(foot step, jump, attack...)

---
+++ Assets/scripts/object/CharacterSound.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSound : MonoBehaviour
+{
+    public AudioClip stepSound;
+    public AudioClip attackSound;
+    public AudioClip jumpSound;
+    public AudioClip hitSound;
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void stepPlay() {
+        if (stepSound == null) {
+            return;
+        }
+        audioSource.PlayOneShot(stepSound);
+    }
+
+    private void attackPlay() {
+        if (attackSound == null) {
+            return;
+        }
+        audioSource.PlayOneShot(attackSound);
+    }
+
+    public void jumpPlay() {
+        if (jumpSound == null) {
+            return;
+        }
+        audioSource.PlayOneShot(jumpSound);
+    }
+
+    public void hitPlay() {
+        if (hitSound == null) {
+            return;
+        }
+
+        audioSource.PlayOneShot(hitSound);
+    }
+}
